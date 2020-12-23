5f0db63316f16cb305f1671757c55314b802f5fe
Tao Peng <ptpttt@gmail.com>
2020-12-23 23:10:35 +0000

    add compile-org-files.el

---
+++ compile-org-files.el
@@ -0,0 +1,15 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-install 'htmlize)
+(message "htmlize installed")
+
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+(find-file "examples.org")
+(message (org-html-export-to-html))
+(find-file "index.org")
+(message (org-html-export-to-html))
